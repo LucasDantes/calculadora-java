da4e20daae5f47ab0dbebe393a8786d76a5203ac
Lucas Dantes <lucas.dantes@hotmail.com>
2026-04-27 18:13:17 -0300

    teste para PR

---
+++ Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+
+    public static double somar(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtrair(double a, double b) {
+        return a - b;
+    }
+}
